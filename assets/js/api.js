c1f63bf29349590776c37705b35dd32f37f1303d
Leonardo Rodrigues <139061019+Leokb0824@users.noreply.github.com>
2024-09-19 21:47:29 -0300

    commit de atualização

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,7 @@
- async function fetchProfileData() {
-    const url = 'https://github.com/Leokb0824/leo_portifolio/blob/main/data/profile.json'
-    const fetching = await fetch(url)
-    return await fetching.json()
+
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/Leokb0824/leo_portifolio/refs/heads/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 }
